abfed3e62590acc368a97eb1a7f4a69e7217d9c7
BernoBelmonte <berbelmonte18@gmail.com>
2024-04-11 20:52:25 -0400

    "AgencyController updated"

--- app/Http/Controllers/AgencyController.php
+++ app/Http/Controllers/AgencyController.php
@@ -8,7 +8,37 @@ class AgencyController extends Controller
 {
     public function indexAction()
     {
-        $agency = Agency::all();
-        return view('agency.index', compact('agency'));
+        $agencies = Agency::all();
+        return view('agencies.index', compact('agencies'));
+    }
+    public function create()
+    {
+        return view('agencies.create');
+    }
+    public function store(Request $request)
+    {
+        $agency = Agency::create($request->all());
+
+        return redirect()->route('agencies.index')->with('success', 'Agency created successfully');
+    }
+    public function show(Agency $agency)
+    {
+        return view('agencies.show', compact('agency'));
+    }
+    public function edit(Agency $agency)
+    {
+        return view('agencies.edit', compact('agency'));
+    }
+    public function update(Request $request, Agency $agency)
+    {
+        $agency->update($request->all());
+
+        return redirect()->route('agencies.index')->with('success', 'Agency updated successfully');
+    }
+    public function destroy(Agency $agency)
+    {
+        $agency->delete();
+
+        return redirect()->route('agencies.index')->with('success', 'Agency deleted successfully');
     }
 }
