ee34c81be3a62a789b7429d7a6523d0406c20334
vincent292 <ariasvincent292@gmail.com>
2024-04-12 18:49:20 -0400

    Corregido inventariosssss

--- database/seeders/InventorySeeder.php
+++ database/seeders/InventorySeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Inventory;
 
-class InventorySeeder extends Seeder
+class InventorySeeder  extends Seeder
 {
     /**
      * Run the database seeds.
