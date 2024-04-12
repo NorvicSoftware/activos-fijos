6dd11f15ba00d5fb00743c66a2acf8c07e2ee1bf
BernoBelmonte <berbelmonte18@gmail.com>
2024-04-12 18:59:41 -0400

    "Changes to the manager table and agency table"

--- database/seeders/AgencySeeder.php
+++ database/seeders/AgencySeeder.php
@@ -15,7 +15,7 @@ class AgencySeeder extends Seeder
     
     public function run()
     { 
-        /*
+        
         Agency::create([
             'name' => 'Acme Agency',
             'address' => '123 Main Street',
@@ -27,7 +27,7 @@ class AgencySeeder extends Seeder
             'address' => '456 Elm Street',
             'phoneNumber' => '666-666-6666'
         ]);
-*/
-        \App\Models\Agency::factory(5)->create(); 
+
+        //\App\Models\Agency::factory(5)->create(); 
     }
 }
